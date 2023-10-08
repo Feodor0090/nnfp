3f07cccebc058a5a9aa1df83e879c5c74092be15
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-10-09 02:44:42 +0300

    Better work with session object

--- NNFPServer/Worker.cs
+++ NNFPServer/Worker.cs
@@ -24,19 +24,36 @@ public class Worker : BackgroundService
             {
                 var accepted = await socket.AcceptTcpClientAsync(stoppingToken);
                 _logger.Log(LogLevel.Information, "Session with {@Client} started", accepted.Client.RemoteEndPoint);
-                var session = new Session(accepted, new CredentialsManager()).RunAsync(stoppingToken).ContinueWith(t =>
-                {
-                    _logger.Log(LogLevel.Information, "Session closed.");
-                    if (t.IsFaulted)
-                    {
-                        _logger.Log(LogLevel.Error, t.Exception.InnerException, "Session handler failed");
-                    }
-                });
+                ProcessConnection(accepted, stoppingToken);
             }
         }
         finally
         {
             socket.Stop();
+            _logger.Log(LogLevel.Information, "Listener closed");
         }
+    }
+
+    private async void ProcessConnection(TcpClient client, CancellationToken cancellationToken)
+    {
+        var session = new Session(client, new CredentialsManager());
+        try
+        {
+            await session.RunAsync(cancellationToken);
+        }
+        catch (OperationCanceledException)
+        {
+            // skip
+        }
+        catch (Exception e)
+        {
+            _logger.Log(LogLevel.Error, e, "Session handler failed");
+        }
+        finally
+        {
+            session.Dispose();
+        }
+
+        _logger.Log(LogLevel.Information, "Session closed");
     }
 }
